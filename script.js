87500fd54c1f564bfb266c72557eac89fcd1b7b7
Alan <alanbuckle05@gmail.com>
2018-09-05 18:24:37 -0400

    More working funtions added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(() => {
     console.log("ReadyPlayerOne")
 
 $("#deal").click(function() {
-    // dealCard(randomCard())
+    
     console.log("Deal Me In")
 })
 
@@ -24,11 +24,11 @@ $("#newgame").click(function() {
 function createDeck () {
     var deck = new Array();
     var suits = ["Diamonds", "Hearts", "Spades", "Clubs"];
-    var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+    var numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
         
     for (var s = 0; s < suits.length; s++) {
-        for (var v = 0; v < values.length; v++) {
-        var card = {Value: values[v], Suit: suits[s]};
+        for (var n = 0; n < numbers.length; n++) {
+        var card = {Number: numbers[n], Suit: suits[s]};
         deck.push(card);
     }
 }
@@ -37,13 +37,14 @@ return deck;
 createDeck();
 console.log("We made a deck");
 
+
+
+
 //Durstenfeld shuffle used to randomize the deck
 function shuffleArray(array) {
     
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
-
-      
         var temp = array[i];
         array[i] = array[j];
         array[j] = temp;
@@ -55,38 +56,61 @@ var arr = [2, 5, 69, 32, 101, 52, 39, 7];
 arr = shuffleArray(arr);
 console.log(arr);
 
+
+//Value system for card
+function cardValue() {
+    if (numbers === "J" || numbers === "Q" || numbers === "K") {
+        return 10
+    }
+    else if (numbers === "A") {
+        return 11 
+    }
+    else {
+        return numbers
+        }        
+    }
+var numbers = ("2");
+numbers = cardValue();
+console.log(cardValue());
+
 //Need to create a function that will deal the hand from the deck
 //Deal cards button will give two face up cards to player
 //Then place the dealt card image/value in each respective box
+//link images with each card
 
 
 //sum total of cards in hand (create value system)
-//if sum is less than 21, player can hit
+
 //Make hit button work to draw the next card in deck
-//if over 21, alert player has busted
 //if stand is clicked the dealer will recieve next card untill equal to 17 or between 18 - 21
 //if dealer goes over 21, bust and player wins
 
 
 
 //Function that alerts player if they can hit, busted or hit Blackjack
-function cardSum() {
-    var cardSum = 15;
+function Alert() {
+    var cardSum = 21;
 if (cardSum > 21) {
-    alert("BUSTED")
+    console.log("BUSTED")
 }
 if (cardSum === 21) {
-    alert("BLACKJACK You Win")
+    console.log("BLACKJACK You Win")
 }
 else console.log("You may hit")
  }
-cardSum();
+Alert();
+
+//Function for deal button
+//Will add two cards to player and two cards to dealer
 
 
 
+//Function for hit button
+//only adds 1 card to player deck and checks to see if its a bust
 
 
 
+//Function for stand button
 
 
 
