67703b5a95b169ecadc1072b871f61c42d3c0514
Alan <alanbuckle05@gmail.com>
2018-09-05 12:01:25 -0400

    Updated playing board and started making functions

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(() => {
     console.log("ReadyPlayerOne")
 
 $("#deal").click(function() {
+    dealCard(randomCard())
     console.log("Deal Me In")
 })
 
@@ -17,9 +18,26 @@ $("#redeal").click(function() {
     console.log("Let's go again")
 })
 
+var deck = new Array ();
+var suits = ["Diamonds", "Hearts", "Spades", "Clubs"];
+var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+function createDeck () {
+    var deck = new Array();
+    for (var i = 0; i < suits.length; i++) {
+        for (var v = 0; v < values.length; v++) {
+            var
+        var card = {value: values[v], suit: suits[i]};
+        deck.push(card);
+    }
+}
+return deck;
+}
 
 
-
+function randomCard() {
+    return Math.floor(Math.random() * numberOfCardsInDeck);
+}
 
 
 
