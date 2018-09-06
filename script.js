7e2ac6ac26021be09a95b83e51f65582681efbf6
Alan <alanbuckle05@gmail.com>
2018-09-06 00:58:31 -0400

    Reorganized functions with a more logic order.Add new code for displaying the cards.

--- script.js
+++ script.js
@@ -18,60 +18,106 @@ $("#newgame").click(function() {
     console.log("Let's go again")
 })
 
+//Work functions in order of logic. Start with cards, then deck and finally hand.  Hand will deal with scores and alerts.
+
+//First wanted to create the cards that will be played
+var suits = ["Diamonds", "Hearts", "Spades", "Clubs"];
+var numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+var deck = new Array();
 
 
-//First wanted to create the deck that will be played and then will randomize it in shuffle before dealing
-function createDeck () {
-    var deck = new Array();
-    var suits = ["Diamonds", "Hearts", "Spades", "Clubs"];
-    var numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-        
+//Found a cool way to display the cards using value and suit symbols
+function faceCard(card) {
+    var el = document.createElement('div');
+    var symbol = " ";
+    if (card.Suit == "Hearts")
+    symbol = "&hearts;";
+    else if (card.Suit == "Diamonds")
+    symbol = "&diams;";
+    else if (card.Suit == "Spades")
+    symbol = "&spades;";
+    else (card.Suit == "Clubs")
+    symbol = "&clubs;";
+
+    el.className = "card";
+    el.innerHTML = card.Value + "<br/>" + symbol;
+    return el;
+    
+}
+
+//Creating the deck and adding values for face cards and Ace.
+//Adding the shuffle function to randomize deck
+
+function makeDeck() {
+    deck = new Array();
     for (var s = 0; s < suits.length; s++) {
         for (var n = 0; n < numbers.length; n++) {
         var card = {Number: numbers[n], Suit: suits[s]};
         deck.push(card);
+        }
     }
 }
-return deck;
-}
-createDeck();
-console.log("We made a deck");
+makeDeck();
 
+//Value for face cards and Ace
+function cardValue() {
+if (numbers === "J" || numbers === "Q" || numbers === "K") {
+    return 10
+}
+else if (numbers === "A") {
+    return 11 
+}
+else {
+    return numbers
+    }        
+}
+
+var numbers = ("A");
+numbers = cardValue();
+console.log(cardValue());
 
 
 
 //Durstenfeld shuffle used to randomize the deck
-function shuffleArray(array) {
+function shuffle(arr) {
     
-    for (var i = array.length - 1; i > 0; i--) {
+    for (var i = arr.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
 
     }
-    return array;
+    return arr;
 }
-var arr = [2, 5, 69, 32, 101, 52, 39, 7];
-arr = shuffleArray(arr);
+var arr = [26, 5, 007, 34, 8, 590, 67, "H", "WE", "ZZ"];
+arr = shuffle(arr);
 console.log(arr);
 
 
-//Value system for card
-function cardValue() {
-    if (numbers === "J" || numbers === "Q" || numbers === "K") {
-        return 10
-    }
-    else if (numbers === "A") {
-        return 11 
-    }
-    else {
-        return numbers
-        }        
-    }
-var numbers = ("2");
-numbers = cardValue();
-console.log(cardValue());
+
+
+//Card images and adding image to game board
+//Add two images to player and then two to dealer
+
+// function displayImg(card) {
+//     for (var i = 0; i < deck.length; i++) {
+//         var imgElement = document.createElement('img')
+//         imgElement.src =  "images/" + card[i].numbers + " of " + card[i].suits
+            
+//             if ()
+//     }
+
+
+
+
+
+
+
+// }
+
+//Need to make a funtion for a card that will pop() from deck and push() card to hand and show its images
+
 
 //Need to create a function that will deal the hand from the deck
 //Deal cards button will give two face up cards to player
