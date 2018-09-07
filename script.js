e6709778ee27ff2a48be5e0f0a0a32b5a0201b92
Alan <alanbuckle05@gmail.com>
2018-09-06 20:21:54 -0400

    deleted unneeded comments and cleaned up code

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-$(() => {
-    makeDeck();
-    shuffle();
-    console.log("ReadyPlayerOne")
-})
+var suits = ["Diamonds", "Hearts", "Spades", "Clubs"];
+var numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+var deck = new Array();
+var players = ['Player0', 'Player1'];
+let playerHand = []
 
-$("#deal").click(function () {
-    dealCards();
-    score();
-})
+
+function makeDeck() {
+    // deck = new Array();
+    for (var s = 0; s < suits.length; s++) {
+        for (var n = 0; n < numbers.length; n++) {
+            var card = { Numbers: numbers[n], Suits: suits[s] };
+            deck.push(card);
+        }
+    }
+    console.log(deck);
+}
+
+
+//Durstenfeld shuffle used to randomize the deck
+
+function shuffle() {
+    for (var i = deck.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+}
+
 
 function dealCards() {
     var card = deck.pop();
@@ -17,20 +37,11 @@ function dealCards() {
     console.log("Dealt", playerHand)
 }
 
-$("#hit").click(function () {
-    hit();
-    score();
-})
-
 function hit() {
     var card = deck.pop();
     playerHand.push(card);
     console.log(playerHand)
 }
-
-$("#stand").click(function () {
-    score();
-})
 
 function score() {
     let sum = 0
@@ -45,8 +56,32 @@ function score() {
     }
 
     console.log('Final hand value: ', sum)
-
 }
+
+
+
+
+$(() => {
+    makeDeck();
+    shuffle();
+})
+
+
+$("#deal").click(function () {
+    dealCards();
+    score();
+})
+
+
+$("#hit").click(function () {
+    hit();
+    score();
+})
+
+
+$("#stand").click(function () {
+    score();
+})
 
 
 $("#newgame").click(function () {
@@ -54,28 +89,7 @@ $("#newgame").click(function () {
 
 })
 
-//Work functions in order of logic. Start with cards, then deck and finally hand.  Hand will deal with scores and alerts.
 
-//First wanted to create the cards that will be played
-var suits = ["Diamonds", "Hearts", "Spades", "Clubs"];
-var numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-var deck = new Array();
-// var players = ['Player0', 'Player1'];
-let playerHand = []
-
-//Creating the deck and adding values for face cards and Ace.
-//Adding the shuffle function to randomize deck
-
-function makeDeck() {
-    // deck = new Array();
-    for (var s = 0; s < suits.length; s++) {
-        for (var n = 0; n < numbers.length; n++) {
-            var card = { Numbers: numbers[n], Suits: suits[s] };
-            deck.push(card);
-        }
-    }
-    console.log(deck);
-}
 
 //Value for face cards and Ace
 // function cardValue() {
@@ -93,7 +107,6 @@ function makeDeck() {
 // var numbers = ("5");
 // numbers = cardValue();
 // console.log(cardValue());
-//Found a cool way to display the cards using value and suit symbols
 
 
 // function faceCard(card) {
@@ -112,77 +125,3 @@ function makeDeck() {
 //     el.innerHTML = card.Value + "<br/>" + symbol;
 //     return el;
 // }
-
-
-//Durstenfeld shuffle used to randomize the deck
-function shuffle() {
-    for (var i = deck.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
-    }
-}
-
-
-
-
-// function score() {
-//     var points = 0;
-//     for (var i = 0; i < player.hand.length; i++) {
-//         points += player.hand;
-//     }
-//     players[i].points = score;
-//     return score;
-// }
-
-//Change the alert function to a means to check if player busted over 21  
-// function checkForBust() {
-//     var sum = 22;
-//     if (sum > 21) {
-//         console.log("BUSTED")
-//     }
-//     if (sum === 21) {
-//         console.log("BLACKJACK You Win")
-//     }
-// }
-
-
-
-
-
-//Need to create a function that will deal the hand from the deck
-//Deal cards button will give two face up cards to player
-//Then place the dealt card image/value in each respective box
-//link images with each card
-
-
-//sum total of cards in hand (create value system)
-
-//Make hit button work to draw the next card in deck
-//if stand is clicked the dealer will recieve next card untill equal to 17 or between 18 - 21
-//if dealer goes over 21, bust and player wins
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// });
-
